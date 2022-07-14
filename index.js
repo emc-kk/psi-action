27c088da055f6e98c3528c6f1214e2571a6948de
emc-kk <emc.kk0314@gmail.com>
2022-07-15 00:52:47 +0900

    debug

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const run = async () => {
 
 
     const performance = output.data.lighthouseResult.categories.performance;
-    core.setOutput("score", JSON.stringify(output.data.lighthouseResult));
+    core.setOutput("score", JSON.stringify(Object.keys(output.data.lighthouseResult).join(',')));
     const coreWebVitals = getCoreWebVitals(performance.auditRefs);
     setCoreWebVitals(coreWebVitals);
   } catch (error) {
