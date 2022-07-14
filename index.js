ec5bcbdf589d017e012b1443d9fdea673960e10c
emc-kk <emc.kk0314@gmail.com>
2022-07-15 00:23:32 +0900

    feat: Adding a parameter 'message' to comment the PSI result once Pull Request occurred

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require("@actions/core");
 const psi = require("psi");
 
+const coreWebVitalsTypes = ["FCP", "TTI", "SI", "TBT", "LCP", "CLS", "FMP"];
+
 const run = async () => {
   try {
     const url = core.getInput("url");
@@ -15,16 +17,34 @@ const run = async () => {
     const strategy = core.getInput("strategy") || "mobile";
     // Output a formatted report to the terminal
     console.log(`Running Page Speed Insights for ${url}`);
-    await psi.output(url, {
+    const output = await psi(url, {
       ...(key ? {key} : undefined),
       ...(key ? undefined : {nokey: "true"}),
       strategy,
       format: "cli",
       threshold
     });
+
+    const performance = output.data.lighthouseResult.categories.performance;
+    core.setOutput("score", JSON.stringify(performance.score));
+    const coreWebVitals = getCoreWebVitals(performance.auditRefs);
+    setCoreWebVitals(coreWebVitals);
   } catch (error) {
     core.setFailed(error.message);
+    core.setOutput("message", String(error.score));
   }
 };
+
+const getCoreWebVitals = (refs) => {
+  return refs.reduce((acc, ref, _) => {
+    if (coreWebVitalsTypes.includes(ref.acronym))
+      acc[ref.acronym] = ref.weight;
+  }, {});
+}
+
+const setCoreWebVitals = (coreWebVitals) => {
+  for (const type of coreWebVitalsTypes) 
+      core.setOutput(type.toLowerCase(), JSON.stringify(coreWebVitals[type]));
+}
 
 run();
